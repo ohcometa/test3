6dd984945e1876a9af011e13bb3c3517dda82d4a
ohcometa <ohcometa@gmail.com>
2020-11-24 16:13:33 +0900

    ver1.3

--- src/test/java/com/jvision/admin/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jvision/admin/domain/posts/PostsRepositoryTest.java
@@ -33,7 +33,7 @@ public void 게시글저장_불러오기(){
     postsRepository.save(Posts.builder()
             .title(title)
             .content(content)
-            .author("2grizzzzzly55@gmail.com")
+            .author("23grizzzzzly55@gmail.com")
             .build()
     );
 

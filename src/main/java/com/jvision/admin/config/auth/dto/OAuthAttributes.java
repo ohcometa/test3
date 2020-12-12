a5acd15b1c113a0452c85f15b95784620041a247
ohcometa <ohcometa@gmail.com>
2020-12-12 22:08:41 +0900

    p14-2 네이버 로그인 등록 2 오류 뜨는거에 대해 OAuthAttributes.java에 주석 달아놨음

--- src/main/java/com/jvision/admin/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jvision/admin/config/auth/dto/OAuthAttributes.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 
 import java.util.Map;
 
+
 @Getter
 public class OAuthAttributes {
     private Map<String, Object> attributes;
@@ -28,8 +29,43 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId, String userNameAttributeName,
                                      Map<String, Object> attributes)
     {
+        //14-2 0:39
+        if("naver".equals(registrationId))
+
+            return ofNaver("id", attributes);
+            //return해서 OfNaver 메소드에 "id"랑 attributes를 매개변수로 넣은 것을 동작시킴
+
+
+
         return ofGoogle(userNameAttributeName, attributes);
     }
+
+
+
+    //14-2 2:04 별 설명은 없고 그냥 response로 받아온다 그정도만 설명하심
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes)
+    {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        //attribute 속성중에 "response"에 해당하는 값을 참조하게 함
+        return OAuthAttributes.builder()
+                .name((String)response.get("name"))
+                .email((String)response.get("email"))
+                .picture((String)response.get("profile_image"))
+                //response가 갖고 있는 name과 email, profile image를
+                //OAuthAttributes의 name email picture에 집어넣게 함 (build의 역할)
+
+                .attributes(response) //attributes는 response를 설정함
+                .nameAttributeKey(userNameAttributeName)
+                //userNameAttributeName는 id값을 의미.
+                //그 id값을 설정을 해서 OAuthAttributes를 빌드해주는 것 (8:08)
+                .build();
+    }
+    //만약 Kakao의 로그인 연동을 쓰고 싶다. 그러면 조건문 하나하고 OfKakao 같은 메소드 하나 필요
+    //여기까지 작성하고 이 OauthAttributes 창에서 Application 실행하면 오류뜨는데,
+    //기능은 정상적으로 다 작동한다
+    //웃기게 다른 창. 예를 들어 index.mustache 창에서 Application 을 실행하면 그 오류가 안뜬다.
+
+
 
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes)
@@ -42,6 +78,9 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
+
+
+
     public User toEntity()
     {
         return User.builder()
@@ -53,3 +92,4 @@ public class OAuthAttributes {
     }
 
 }
+
